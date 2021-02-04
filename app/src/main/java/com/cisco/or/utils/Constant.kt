82c4141188133bb7197cecf6008fe7b171afb706
Binish Balakrishnan <binbalak@cisco.com>
2021-02-04 20:24:40 +0530

    Changed the google client id

--- app/src/main/java/com/cisco/or/utils/Constant.kt
+++ app/src/main/java/com/cisco/or/utils/Constant.kt
@@ -10,7 +10,7 @@ class Constant {
         val ApplicationId = "com.cisco.fit"
         var PUSH_TOKEN = "push_token"
         val dnaSpacesKey = "2bba6f2b-2399-4246-8257-fcc6c95ba337"
-        val SERVER_CLIENT_ID = "1005366675234-8caio4nluef6nj7dg5ubs0hc1ll9t222.apps.googleusercontent.com"
+        val SERVER_CLIENT_ID = "698867933682-8e8eq5ahe1pfe1bt0hkpjpc29gu8d540.apps.googleusercontent.com"
      }
 }
 
